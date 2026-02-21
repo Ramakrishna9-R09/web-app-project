bf2928aff875c1384c39531b7ed46fac68b9c823
22MIS1167 <ramakrishna.chowdary2005@gmail.com>
2026-02-21 10:52:01 +0530

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCm1WBAAcphr9B19WhILQqHAKFmx_p4jPY",
+  apiKey: "",
   authDomain: "vit-connect-420a2.firebaseapp.com",
   projectId: "vit-connect-420a2",
   storageBucket: "vit-connect-420a2.appspot.com",
